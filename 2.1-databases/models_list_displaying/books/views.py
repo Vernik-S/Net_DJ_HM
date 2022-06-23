f47155e39431a149abc179fd5b1f57b391dfbca3
Vernik-S <98013545+Vernik-S@users.noreply.github.com>
2022-06-23 18:21:44 +0300

    2.2 Catalog works

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from datetime import datetime
+from http.client import HTTPResponse
 
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from books.models import Book
+
+
+def index(request):
+    return redirect('books')
 
 def books_view(request):
-    template = 'books/books_list.html'
-    context = {}
+    #template = 'books/books_list.html'
+    template = 'books/books_list_my.html'
+    book_objects = Book.objects.all()
+    context = {
+        "books" : book_objects
+    }
     return render(request, template, context)
+
+def date_view(request, date):
+    date_time_obj = datetime.strptime(date, '%Y-%m-%d' ) #
+    return HttpResponse(date_time_obj)
