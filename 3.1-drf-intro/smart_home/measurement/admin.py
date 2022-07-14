e4c8837580323ff4e48c9996a3179469859930de
Vernik-S <vernik.krd@gmail.com>
2022-07-14 23:07:43 +0300

    3.1 Models and admin added

--- 3.1-drf-intro/smart_home/measurement/admin.py
+++ 3.1-drf-intro/smart_home/measurement/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from measurement.models import Measurement, Sensor
+
+
+class MeasurementInline(admin.TabularInline):
+    model = Measurement
+    extra = 1
+
+
+@admin.register(Sensor)
+class SensorAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", ]
+    inlines = [MeasurementInline]
+
+
+@admin.register(Measurement)
+class MeasurementAdmin(admin.ModelAdmin):
+    list_display = ["sensor", "temperature", "date_m", ]
--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Имя датчика")
+    description = models.CharField(max_length=100, verbose_name="Описание датчика")
+
+    def __str__(self):
+        return self.name
+
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name="measurements")
+    temperature = models.DecimalField(max_digits=5, decimal_places=2)
+    date_m = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.sensor)+":"+str(self.date_m)
+
+
