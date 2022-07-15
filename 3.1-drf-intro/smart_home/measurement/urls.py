44e1839482752045ed4b23eee649b299d1e10c3f
Vernik-S <98013545+Vernik-S@users.noreply.github.com>
2022-07-15 16:19:32 +0300

    3.1 PATCH sesnors works

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from measurement.views import SensorsView
+from measurement.views import SensorsView, SensorView
 
 urlpatterns = [
     # TODO: зарегистрируйте необходимые маршруты
     path('sensors/', SensorsView.as_view()),
+    path('sensors/<pk>/', SensorView.as_view()),
 
 ]
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,15 +1,21 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
-from rest_framework.generics import ListAPIView, RetrieveAPIView, ListCreateAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, ListCreateAPIView, CreateAPIView, \
+    RetrieveUpdateAPIView
 from rest_framework.response import Response
 
 from measurement.models import Sensor
 from measurement.serializers import SensorsSerializer
 
 
-class SensorsView(ListCreateAPIView):
+class SensorsView(ListCreateAPIView, ):
     queryset = Sensor.objects.all()
     serializer_class = SensorsSerializer
+
+class SensorView(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorsSerializer
+
 
 # class SensorView(CreateAPIView):
 #     queryset = Sensor.objects.all()
