74f1f3be1d99d033059ef6b5b524980b64c3b79a
Vernik-S <98013545+Vernik-S@users.noreply.github.com>
2022-07-15 18:43:57 +0300

    3.1 Additional taks photos

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class Sensor(models.Model):
     name = models.CharField(max_length=50, verbose_name="Имя датчика")
     description = models.CharField(max_length=100, verbose_name="Описание датчика")
+    photo = models.ImageField(null=True, verbose_name="Фото", upload_to="files/sensors_photos")
 
     def __str__(self):
         return self.name
@@ -14,8 +15,8 @@ class Sensor(models.Model):
 
 class Measurement(models.Model):
     sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name="measurements")
-    temperature = models.DecimalField(max_digits=5, decimal_places=2)
-    date_m = models.DateField(auto_now_add=True)
+    temperature = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Температура")
+    date_m = models.DateField(auto_now_add=True, verbose_name="Дата измерения")
 
     def __str__(self):
         return str(self.sensor)+":"+str(self.date_m)
