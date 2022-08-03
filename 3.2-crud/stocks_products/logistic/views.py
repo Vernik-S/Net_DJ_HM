6228d30746177ac7e5278fe5fd48d29f30a072ad
Vernik-S <98013545+Vernik-S@users.noreply.github.com>
2022-08-04 00:06:35 +0300

    3.2 Custom filter and requirements

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -17,13 +17,15 @@ class ProductViewSet(ModelViewSet):
     pagination_class = PageNumberPagination
     pagination.PageNumberPagination.page_size = 2
 
+class StockCustomFilter(SearchFilter):
+    search_param = "products_search"
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     # при необходимости добавьте параметры фильтрации
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, StockCustomFilter]
     filterset_fields = ['id', 'products', ]
     search_fields = ['products__title', 'products__description']
-    SearchFilter.search_param = "products_search" #для допзадания. Параметр поиска был изменен чтобы не было конфликта с поиском по id продукта
+    #SearchFilter.search_param = "products_search" #для допзадания. Параметр поиска был изменен чтобы не было конфликта с поиском по id продукта
     pagination_class = LimitOffsetPagination
