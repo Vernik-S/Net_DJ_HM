7d45a3dca9b1b3122509bc91888962358124399e
Vernik-S <98013545+Vernik-S@users.noreply.github.com>
2022-08-10 15:00:40 +0300

    3.3 Initiial settings and router

--- 3.3-permissions/api_with_restrictions/advertisements/filters.py
+++ 3.3-permissions/api_with_restrictions/advertisements/filters.py
@@ -1,12 +1,15 @@
-from django_filters import rest_framework as filters
+from django_filters import rest_framework as filters, DateFromToRangeFilter, ChoiceFilter
 
-from advertisements.models import Advertisement
+from advertisements.models import Advertisement, AdvertisementStatusChoices
 
 
 class AdvertisementFilter(filters.FilterSet):
     """Фильтры для объявлений."""
 
     # TODO: задайте требуемые фильтры
+    updated_at = DateFromToRangeFilter()
+    status = ChoiceFilter(choices=AdvertisementStatusChoices.choices)
 
     class Meta:
         model = Advertisement
+        fields = ["updated_at", "status"]
--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,5 +1,10 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+
+from advertisements.filters import AdvertisementFilter
+from advertisements.models import Advertisement
+from advertisements.serializers import AdvertisementSerializer
 
 
 class AdvertisementViewSet(ModelViewSet):
@@ -7,6 +12,11 @@ class AdvertisementViewSet(ModelViewSet):
 
     # TODO: настройте ViewSet, укажите атрибуты для кверисета,
     #   сериализаторов и фильтров
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    #filter_backends = (DjangoFilterBackend,)
+    #filterset_class = AdvertisementFilter
+
 
     def get_permissions(self):
         """Получение прав для действий."""
