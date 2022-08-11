9417a0a4dfbfa5fdf0b9267e8fe9350e6c7c866d
Vernik-S <98013545+Vernik-S@users.noreply.github.com>
2022-08-11 11:38:55 +0300

    3.3 Draft works

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -4,8 +4,9 @@ from rest_framework.viewsets import ModelViewSet
 
 from advertisements.filters import AdvertisementFilter
 from advertisements.models import Advertisement
-from advertisements.permissions import IsOwnerOrAdmin
+from advertisements.permissions import IsOwnerOrAdmin, IsNotDraft
 from advertisements.serializers import AdvertisementSerializer
+from django.db.models import Q
 
 
 class AdvertisementViewSet(ModelViewSet):
@@ -18,11 +19,19 @@ class AdvertisementViewSet(ModelViewSet):
     filter_backends = (DjangoFilterBackend,)
     filterset_class = AdvertisementFilter
 
-
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create",]:
+        if self.action in ["create", ]:
             return [IsAuthenticated()]
         elif self.action in ["update", "partial_update", "destroy"]:
             return [IsOwnerOrAdmin()]
-        return []
+        return [(IsNotDraft | IsOwnerOrAdmin)()]  # доп задание DRAFT
+
+    def get_queryset(self):  # доп задание DRAFT
+        if self.request.user.is_staff:
+            return Advertisement.objects.all() #админам показываются все объявления
+        else:
+            qs = Advertisement.objects.filter(
+                ~Q(status='DRAFT') | Q(creator=self.request.user.id) #юзерам показываются все нечерновики и все объявления юзера
+            )
+            return qs
