2cc5361f048ad123dd3dd3fec46b4b5a35e4f5ed
Vernik-S <vernik.krd@gmail.com>
2022-09-18 01:04:33 +0300

    3.4 id_filter works

--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,4 +1,5 @@
 import dataclasses
+import random
 
 import pytest
 from rest_framework.test import APIClient
@@ -57,3 +58,23 @@ def test_list_courses(client, course_factory):
 
     for i, response_course in enumerate(data):
         assert response_course['name'] == courses_test[i].name
+
+
+@pytest.mark.django_db
+def test_id_filter_courses(client, course_factory):
+    # Arrange
+    courses_test = course_factory(_quantity=10)
+    random_course = random.choice(courses_test)
+
+
+    # Act
+    response = client.get(f'/api/v1/courses/?id={random_course.id}', follow=True)
+
+    # Assert
+    assert response.status_code == 200
+    data = response.json()
+
+    assert data[0]
+
+    assert data[0]["name"] == random_course.name
+
