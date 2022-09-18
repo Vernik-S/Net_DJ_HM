31f7ef8864a9874c376bb9327e0d5c2739dbf7f2
Vernik-S <vernik.krd@gmail.com>
2022-09-18 10:50:27 +0300

    3.4 Create course test works

--- 3.4-django-testing/django_testing/students/admin.py
+++ 3.4-django-testing/django_testing/students/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from students.models import Course
+from students.models import Course, Student
 
 
 @admin.register(Course)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+
+
+
+@admin.register(Student)
 class ProductAdmin(admin.ModelAdmin):
     pass
--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,10 +1,12 @@
 import dataclasses
+import json
 import random
+from pprint import pprint
 
 import pytest
 from rest_framework.test import APIClient
 
-from students.models import Course
+from students.models import Course, Student
 from model_bakery import baker
 
 
@@ -16,12 +18,13 @@ from model_bakery import baker
 def client():
     return APIClient()
 
+
 @pytest.fixture
 def course_factory():
     def factory(*args, **kwargs):
-        return  baker.make(Course, *args, **kwargs)
-    return factory
+        return baker.make(Course, *args, **kwargs)
 
+    return factory
 
 
 @pytest.mark.django_db
@@ -34,11 +37,11 @@ def test_one_course(client, course_factory):
     response = client.get(f'/api/v1/courses/{id_course}/', follow=True)
 
     # Assert
-    #assert course_test.id == 1
-    #print(course_test.name)
+    # assert course_test.id == 1
+    # print(course_test.name)
     assert response.status_code == 200
     data = response.json()
-    #assert  data["name"] == "random name"
+    # assert  data["name"] == "random name"
 
     assert data["name"] == course_test.name
 
@@ -68,7 +71,6 @@ def test_id_filter_courses(client, course_factory):
     courses_test = course_factory(_quantity=10)
     random_course = random.choice(courses_test)
 
-
     # Act
     response = client.get(f'/api/v1/courses/?id={random_course.id}', follow=True)
 
@@ -80,12 +82,12 @@ def test_id_filter_courses(client, course_factory):
 
     assert data[0]["name"] == random_course.name
 
+
 @pytest.mark.django_db
 def test_name_filter_courses(client, course_factory):
     # Arrange
     courses_test = course_factory(_quantity=10)
     random_course = random.choice(courses_test)
-
 
     # Act
     response = client.get(f'/api/v1/courses/?name={random_course.name}', follow=True)
@@ -97,3 +99,45 @@ def test_name_filter_courses(client, course_factory):
     assert data[0]
 
     assert data[0]["id"] == random_course.id
+
+
+# @pytest.fixture
+# def test_students():
+#     test_student1 = Student.objects.create(name="Test Student 1")
+#     test_student2 = Student.objects.create(name="Test Student 2")
+#     return [test_student1, test_student2]
+
+
+@pytest.fixture
+def student_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Student, *args, **kwargs)
+
+    return factory
+
+
+@pytest.mark.django_db
+def test_create_course(client, student_factory):
+    # Arrange
+
+    students_test = student_factory(_quantity=10)
+
+
+    test_course_body = {
+        "name": "Test Course",
+        "students": [student_test.id for student_test in students_test]
+    }
+    test_course_json = json.dumps(test_course_body)
+
+    #pprint(test_course_json)
+
+    # Act
+    response = client.post(f'/api/v1/courses/', data=test_course_json, follow=True, content_type="application/json")
+    #pprint(response.json())
+
+    # Assert
+    assert response.status_code == 201
+    data = response.json()
+    pprint(data)
+    assert data["name"] == "Test Course"
+    assert len(data["students"]) == len(students_test)
